59df39304530df742e6ddeeac22b8aeadc31ae81
notken12 <kendotzhou@gmail.com>
2023-02-21 20:54:39 -0500

    initial commit

---
+++ index.mjs
@@ -0,0 +1,74 @@
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'node:path';
+import { config } from 'dotenv';
+
+config();
+
+const input = readFileSync(resolve(process.env.INPUT || 'input.txt')).toString();
+const outputDir = process.env.OUTPUT_DIR || './output/';
+
+const tickers = input.split('\n');
+
+const requestInterval = 60 / 200 * 1.1;
+
+const delay = s => new Promise(resolve => setTimeout(resolve, s * 1000))
+
+const fetchData = async (ticker, page_token) => {
+    const paramsInit = {
+        start: process.env.START_DATE,
+        end: process.env.END_DATE,
+        limit: process.env.LIMIT,
+        timeframe: process.env.TIMEFRAME || '1Min'
+    };
+    if (page_token) {
+        paramsInit.page_token = page_token
+    }
+
+    const res = await fetch(`https://data.alpaca.markets/v2/stocks/${ticker}/bars?` + new URLSearchParams(paramsInit), {
+        headers: new Headers({
+            "APCA-API-KEY-ID": process.env.APCA_API_KEY_ID,
+            "APCA-API-SECRET-KEY": process.env.APCA_API_SECRET_KEY
+        })
+    }).catch((e) => {
+        console.log("Error fetching bars: " + e);
+    });
+
+    const body = await res.json();
+
+    if (!('bars' in body)) {
+        console.log('Error fetching bars, response body: ' + JSON.stringify(body));
+        return;
+    }
+
+    return body;
+}
+
+for (const ticker of tickers) {
+    let body = await fetchData(ticker);
+    await delay(requestInterval); // a bit less than the limit of 200 requests/min
+    console.log('successfully fetched bars')
+    let initial = true;
+
+    while (body != null) {
+        let encoded = '';
+        for (const bar of body.bars) {
+            const { t, o, h, l, c, v, n, vw } = bar;
+            encoded += `t${t} o${o} h${h} l${l} c${c} v${v} n${n} vw${vw}\n`;
+        }
+        writeFileSync(resolve(outputDir, `${ticker}.txt`), encoded, {
+            flag: initial ? undefined : "a+"
+        });
+
+        if (body.next_page_token) {
+            await delay(requestInterval); // a bit less than the limit of 200 requests/min
+            body = await fetchData(ticker, body.next_page_token)
+            console.log('successfully fetched bars')
+        }
+        else
+            break;
+        initial = false;
+    }
+    await delay(requestInterval); // a bit less than the limit of 200 requests/min
+}
+
+console.log('done')
